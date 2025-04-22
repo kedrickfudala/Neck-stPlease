98e08f74d65db6869a88c5493b08ad86428d53ce
tangirene <153215728+tangirene@users.noreply.github.com>
2025-04-22 14:03:14 -0400

    added diag balloon to play scene

--- assets/game container/game_container.gd
+++ assets/game container/game_container.gd
@@ -10,8 +10,14 @@ func _ready():
 func spawn_main_menu():
 	var main_menu_inst = main_menu_scene.instantiate()
 	add_child(main_menu_inst)
+	"""print("here")
+	if Input.is_action_just_pressed("ui_accept"):
+		print("here")
+		DialogueManager.show_example_dialogue_balloon(load("res://dialogue/dialogue.dialogue"),"START")
+		return"""
 
 func spawn_room():
 	var room_inst = room_scene.instantiate()
 	room_inst.global_position = Vector2(0,0)
 	add_child(room_inst)
+	DialogueManager.show_example_dialogue_balloon(load("res://dialogue/dialogue.dialogue"),"START")
